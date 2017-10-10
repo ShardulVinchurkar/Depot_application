3ff814b81bda775e7e59d80218a454c528066fd2
ShardulVinchurkar <sharduljv@gmail.com>
2017-10-10 16:56:22 -0400

    Error handling completed

--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -42,7 +42,6 @@ class CartsControllerTest < ActionDispatch::IntegrationTest
     assert_difference('Cart.count', -1) do
       delete cart_url(@cart)
     end
-
     assert_redirected_to carts_url
   end
 end
